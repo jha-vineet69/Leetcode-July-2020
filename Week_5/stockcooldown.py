16ed2cd1ec4ab6f5ad26f43669b34a5006e97bc9
Vineet Jha <vineetjha3388@gmail.com>
2020-07-30 10:29:49 +0530

    Solved Question 29

--- Week_5/stockcooldown.py
+++ Week_5/stockcooldown.py
@@ -1,12 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices)<=1:
+        hm = {}
+        return self.findprofit(0, 0, prices, hm)
+    
+    def findprofit(self, i, bos, prices, hm):
+        if i>= len(prices):
             return 0
-        A, B, C = 0, -prices[0], 0
-        for i in range(1, len(prices)):
-            tmp = A
-            A = max(A, C)
-            C = B + prices[i]
-            B = max(B, tmp-prices[i])
-        return max(A, C)
+        key = str(i) + " " + str(bos)
+        
+        if key in hm:
+            return hm[key]
+        
+        profit = 0
+        
+        if bos == 0:
+            buy = self.findprofit(i+1, 1, prices, hm) - prices[i]
+            nobuy = self.findprofit(i+1, 0, prices, hm)
+            profit = max(buy, nobuy)
+        
+        else:
+            sell = self.findprofit(i+2, 0, prices, hm) + prices[i]
+            nosell = self.findprofit(i+1, 1, prices, hm)
+            profit = max(sell, nosell)
+        
+        hm[key] = profit
+        
+        return profit
+        
         
